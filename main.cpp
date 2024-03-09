0da84a593e0eec0cb5b8abadaa9d132566b27e2b
nicoorourke20 <157660744+nicoorourke20@users.noreply.github.com>
2024-03-08 22:18:18 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,24 @@ using namespace std;
 
 int main()
 {
-	int n1, n2;
-	int N;
+    int n1, n2;
+    int N;
 
-	cin >> n1 >> n2;
-	cin >> N;
+    cin >> n1 >> n2;
+    cin >> N;
 
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
+    // Print the first two initial values
+    cout << n1 << " " << n2 << " ";
+
+    // Generate and print the rest of the sequence
+    for (int i = 2; i < N; i++) {
+        int next = n1 + n2;
+        cout << next << " ";
+        n1 = n2;
+        n2 = next;
+    }
+
+    cout << endl;
+
+    return 0;
 }
